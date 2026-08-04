6b1c9cd969ae8a3c970986943e7b51dacf28cba8
Kuro0000 <andreayang531@gmail.com>
2026-08-04 12:53:20 +0200

    an http server for prometheus

--- kafka.c
+++ kafka.c
@@ -386,7 +386,37 @@ void *clientHandler(void *arg) {
 
 
 void *metricsHandler(void *args){
+    int server_fd, client_fd;
+    struct sockaddr_in address;
+    int opt=1;
+    socklen_t addrlen =sizeof(address);
 
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) 
+        exit(EXIT_FAILURE);
+    
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt))) 
+        exit(EXIT_FAILURE);
+
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(9100);
+
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address))<0) 
+    exit(EXIT_FAILURE);
+    
+    if (listen(server_fd, 3) < 0) exit(EXIT_FAILURE);
+
+    char response[1024];
+    int offset = 0;
+    for(;;) {
+        if ((client_fd =accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen))< 0) 
+            continue;
+
+ 
+        write(client_fd, response, strlen(response));
+        close(client_fd);
+    }
+    return NULL;
 }
 
 
